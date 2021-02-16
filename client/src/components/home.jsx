117afa2bcf04bea7d9c999da49827ed4e25ac3a0
osho <oshosingh25@gmail.com>
2021-02-16 14:54:23 +0530

    coding home component

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -15,6 +15,21 @@ export default function Home(props) {
     `;
 
     const {loading, data, error } = useQuery(GET_USERS);
+    let usersMarkup
+    if(!data || loading){
+        usersMarkup = <p> Loading .....</p>
+    }
+    else if(data.getUsers.length===0){
+        usersMarkup = <p> No users have joined yet</p>
+    }
+    else{
+        usersMarkup = data.getUsers.map( (found) => {
+            return(
+            <div key={found.username}>
+                <p>  {found.username}</p>
+            </div>)
+        } )
+    }
 
     const dispatch = useAuthDispatch()
     const handleLogout = (e) => {
@@ -32,6 +47,15 @@ export default function Home(props) {
                     <Button variant="link"> Register</Button>
                 </Link>
                 <Button variant="link" onClick={handleLogout}> Logout</Button>
+            </Row>
+
+            <Row className="bg-white mt-2 ">
+                <Col xs={4}>
+                    {usersMarkup}
+                </Col>
+                <Col xs={8}>
+                    <p> Messages </p>
+                </Col>
             </Row>
         </>
     )
